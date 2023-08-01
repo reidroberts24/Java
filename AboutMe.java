91b0458f88fd0d153d00ecf78ab697715067de1b
reidroberts24 <116128525+reidroberts24@users.noreply.github.com>
2023-07-31 20:52:38 -0500

    created AboutMe class

---
+++ AboutMe.java
@@ -0,0 +1,7 @@
+public class AboutMe {
+    public static void main(String[] args) {
+        System.out.println("My name is Reid Roberts.");
+        System.out.println("I am 26 years old.");
+        System.out.println("My hometown is Houston, TX");
+    }
+}
