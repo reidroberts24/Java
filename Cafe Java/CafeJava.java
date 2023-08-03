4331eb21ae66c1acf89bd07712b542342f7b5a71
reidroberts24 <116128525+reidroberts24@users.noreply.github.com>
2023-08-02 19:44:58 -0500

    fixed punctuation

--- Cafe Java/CafeJava.java
+++ Cafe Java/CafeJava.java
@@ -21,31 +21,31 @@ public class CafeJava {
     
         // Order completions (add yours below)
         boolean isReadyOrder1 = false;
-        boolean isReadyOrder2 = true;
-        boolean isReadyOrder3 = false;
-        boolean isReadyOrder4 = true;
+        boolean isReadyOrder2 = false;
+        boolean isReadyOrder3 = true;
+        boolean isReadyOrder4 = false;
 
     
         // APP INTERACTION SIMULATION (Add your code for the challenges below)
         // Example:
         System.out.println(generalGreeting + customer1); // Displays "Welcome to Cafe Java, Cindhuri"
     	// ** Your customer interaction print statements will go here ** //
-        System.out.println(customer1 + pendingMessage);
+        System.out.println(customer1 + pendingMessage + "!");
         if (isReadyOrder4) {
-            System.out.println(generalGreeting + customer4 + readyMessage + displayTotalMessage + cappuccino);
+            System.out.println(generalGreeting + customer4 + readyMessage + "! " + displayTotalMessage + cappuccino);
         } else {
-            System.out.println(generalGreeting + customer4 + pendingMessage);
+            System.out.println(generalGreeting + customer4 + pendingMessage + ".");
         }
         
         double samPrice = 2 * latte;
-        System.out.println(generalGreeting + customer2 + displayTotalMessage + samPrice);
+        System.out.println(generalGreeting + customer2 + "! " + displayTotalMessage + samPrice + ".");
         if (isReadyOrder2) {
-            System.out.println(customer2 + readyMessage);
+            System.out.println(customer2 + readyMessage + ".");
         } else {
-            System.out.println(customer2 + pendingMessage);
+            System.out.println(customer2 + pendingMessage + ".");
         }
         
         double jimmyPrice = latte - dripCoffee;
-        System.out.println(generalGreeting + customer3 + displayTotalMessage + jimmyPrice);
+        System.out.println(generalGreeting + customer3 + "! " +displayTotalMessage + jimmyPrice +".");
     }
 }
