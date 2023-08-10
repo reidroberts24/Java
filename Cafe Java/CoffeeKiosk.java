f9e121d0ab9083d4eeb8958d40623b3c2ebd5d41
Reid Roberts <reidrobertscs@gmail.com>
2023-08-09 20:52:46 -0500

    finished coffee kiosk

--- Cafe Java/CoffeeKiosk.java
+++ Cafe Java/CoffeeKiosk.java
@@ -25,29 +25,43 @@ public class CoffeeKiosk {
 
     public void newOrder() {
         
-    	// Shows the user a message prompt and then sets their input to a variable, name
+        // Shows the user a message prompt and then sets their input to a variable, name
         System.out.println("Please enter customer name for new order:");
         String name = System.console().readLine();
     
-    	// Your code:
+        // Your code:
         // Create a new order with the given input string
         Order order1 = new Order(name);
+        orders.add(order1);
+
         // Show the user the menu, so they can choose items to add.
-        System.out.println("Please browse the menu below!");
+        System.out.println("Please browse the menu below! Press q to quit:");
         displayMenu();
         
-    	// Prompts the user to enter an item number
+        // Prompts the user to enter an item number
         System.out.println("Please enter a menu item index or q to quit:");
         String itemNumber = System.console().readLine();
         
         // Write a while loop to collect all user's order items
         while(!itemNumber.equals("q")) {
-            
+            int index = Integer.parseInt(itemNumber);
+            order1.addItem(menu.get(index));
             // Get the item object from the menu, and add the item to the order
             // Ask them to enter a new item index or q again, and take their input
+            System.out.println("Please enter another menu item index or press q to quit:");
+            itemNumber = System.console().readLine();
         }
         // After you have collected their order, print the order details 
-        
+        order1.display();
+    }
+    public static void main(String[] args) {
+        CoffeeKiosk kiosk = new CoffeeKiosk();
+        kiosk.addMenuItem("Latte", 4.50);
+        kiosk.addMenuItem("Cappuccino", 3.50);
+        kiosk.addMenuItem("Espresso", 2.50);
+
+        kiosk.newOrder();
+
     }
 
 }
