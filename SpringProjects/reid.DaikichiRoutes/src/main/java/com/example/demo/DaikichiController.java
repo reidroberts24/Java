ebb3a22b4d86a0ff7664d1db7011344f674b241a
Reid Roberts <reidrobertscs@gmail.com>
2023-08-12 09:08:41 -0500

    added path variables

--- SpringProjects/reid.DaikichiRoutes/src/main/java/com/example/demo/DaikichiController.java
+++ SpringProjects/reid.DaikichiRoutes/src/main/java/com/example/demo/DaikichiController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,4 +29,21 @@ public class DaikichiController {
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
 	}
+	
+	@RequestMapping("/travel/{city}")
+	public String travelCity(@PathVariable("city") String cityName) {
+		return "Congratulations! You will soon travel to " + cityName + "!";
+	}
+	
+	@RequestMapping("/lotto/{num}")
+	public String lotto(@PathVariable("num") int num) {
+		String result;
+		if (num % 2 == 0) {
+			result = "You will take a grand journey in the near future, but be weary of tempting offers.";
+		} else {
+			result = "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+		}
+		return result;
+	}
+	
 }
